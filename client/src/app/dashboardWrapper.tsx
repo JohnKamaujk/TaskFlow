eeecdce1603fdf5ee1ef2881ff6a679f621f83e8
JohnKamaujk <johnjkkamau@gmail.com>
2024-09-26 23:35:18 +0300

    :sparkles:chore:app layout

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+        {/* {sidebar} */}
+        sidebar
+        <main className="dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64">
+          {/* Navbar */}
+          Navbar
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
