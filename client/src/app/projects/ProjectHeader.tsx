6287ed9c98944bfe9a8195e1c930ae143cd015e4
JohnKamaujk <johnjkkamau@gmail.com>
2024-10-06 13:46:05 +0300

    :sparkles:feat: pages header

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/Header";
+import { PlusSquare } from "lucide-react";
 import React from "react";
 
 type Props = {
@@ -6,7 +8,20 @@ type Props = {
 };
 
 const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
-  return <div>ProjectHeader</div>;
+  return (
+    <div className="px-4 xl:px-6">
+      <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
+        <Header
+          name="Product Design Development"
+          buttonComponent={
+            <button className="flex items-center rounded-md bg-blue-primary px-3 py-2 text-white hover:bg-blue-600">
+              <PlusSquare className="mr-2 h-5 w-5" /> New Boards
+            </button>
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ProjectHeader;
