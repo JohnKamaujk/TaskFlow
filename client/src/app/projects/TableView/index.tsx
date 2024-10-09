76883f543285c32b2dbd64ab94151655753de91c
JohnKamaujk <johnjkkamau@gmail.com>
2024-10-09 22:39:48 +0300

    :sparkles:feat:Table View

--- client/src/app/projects/TableView/index.tsx
+++ client/src/app/projects/TableView/index.tsx
@@ -1,8 +1,22 @@
+import { useAppSelector } from "@/app/redux";
+import { useGetTasksQuery } from "@/state/api";
 import React from "react";
 
-type Props = {};
+type Props = {
+  id: string;
+  setIsModalNewTaskOpen: (isOpen: boolean) => void;
+};
 
-const TableView = (props: Props) => {
+const TableView = ({ id, setIsModalNewTaskOpen }: Props) => {
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  const {
+    data: tasks,
+    error,
+    isLoading,
+  } = useGetTasksQuery({ projectId: Number(id) });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error || !tasks) return <div>An error occurred while fetching tasks</div>;
   return <div>TableView</div>;
 };
 
