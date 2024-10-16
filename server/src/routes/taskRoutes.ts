2844090972a76b7e449523a35a377653ecc289eb
JohnKamaujk <johnjkkamau@gmail.com>
2024-10-16 22:11:56 +0300

    :sparkles:feat: get user's tasks route

--- server/src/routes/taskRoutes.ts
+++ server/src/routes/taskRoutes.ts
@@ -3,11 +3,13 @@ import {
   getTasks,
   createTask,
   updateTaskStatus,
+  getUserTasks,
 } from "../controllers/taskController";
 
 const router = Router();
 
 router.get("/", getTasks);
+router.get("/user/:userId", getUserTasks);
 router.post("/", createTask);
 router.patch("/:taskId/status", updateTaskStatus);
 
